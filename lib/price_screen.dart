8f9c13d40e578a90886ae08fe698d9f5de2c4d0b
Vetri <37167788+Vetrivelkv@users.noreply.github.com>
2022-07-09 12:46:54 +0530

    dropDownButton

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -6,6 +6,8 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,12 +40,32 @@ class _PriceScreenState extends State<PriceScreen> {
             ),
           ),
           Container(
-            height: 150.0,
-            alignment: Alignment.center,
-            padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: null,
-          ),
+              height: 150.0,
+              alignment: Alignment.center,
+              padding: EdgeInsets.only(bottom: 30.0),
+              color: Colors.lightBlue,
+              child: DropdownButton<String>(
+                value: selectedCurrency,
+                items: [
+                  DropdownMenuItem(
+                    child: Text('USD'),
+                    value: 'USD',
+                  ),
+                  DropdownMenuItem(
+                    child: Text('EUR'),
+                    value: 'EUR',
+                  ),
+                  DropdownMenuItem(
+                    child: Text('GBP'),
+                    value: 'GBP',
+                  )
+                ],
+                onChanged: (value) {
+                  setState(() {
+                    selectedCurrency = value.toString();
+                  });
+                },
+              )),
         ],
       ),
     );
